e5f66cdc6680236072b78ac9795e16a23fa40268
UBOTEAS <asankaboteju@gmail.com>
2018-03-11 08:53:17 +0530

    Code Implementation
    Code Implementation

---
+++ src/main/java/com/asanka/eurekasvc/Eurekasvcone1Application.java
@@ -0,0 +1,14 @@
+package com.asanka.eurekasvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Eurekasvcone1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eurekasvcone1Application.class, args);
+	}
+}
